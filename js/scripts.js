a5d2d005ca4ac6c99391741cd39f3b4c426a0934
tblaney <104526946+traegis@users.noreply.github.com>
2023-01-26 00:19:22 -0800

    changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 /*!
-* Start Bootstrap - Freelancer v7.0.2 (https://startbootstrap.com/theme/freelancer)
-* Copyright 2013-2021 Start Bootstrap
+* Start Bootstrap - Freelancer v7.0.6 (https://startbootstrap.com/theme/freelancer)
+* Copyright 2013-2022 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-freelancer/blob/master/LICENSE)
 */
 //
@@ -51,45 +51,4 @@ window.addEventListener('DOMContentLoaded', event => {
         });
     });
 
-    /*document.getElementById("closeButton").addEventListener("click", stopVideo);*/
-    /*document.getElementsByClassName("circles").addEventListener("click", stopVideo);*/
-    /*document.getElementsByClassName("circles").addEventListener("click", stopVideo);*/
-    /*document.getElementsByClassName("close-button1").addEventListener("click", stopVideo1);*/
-    /*document.querySelector('#closeButton1').addEventListener("click", stopVideo1);*/
-    /*document.getElementById("closeButton1.1").addEventListener("click", stopVideo1);*/
-    /*document.querySelectorAll("#closeButton1.1, #closeButton1.2").addEventListener("click", stopVideo1);*/
-    document.getElementById("closeButton1.1").addEventListener("click", stopVideo1);
-    document.getElementById("closeButton1.2").addEventListener("click", stopVideo1);
-    var video1 = document.getElementById("myVideoPlayer1");
-     function stopVideo1() {
-          video1.pause();
-          video1.currentTime = 0;
-     };
-
-    document.getElementById("closeButton2.1").addEventListener("click", stopVideo2);
-    document.getElementById("closeButton2.2").addEventListener("click", stopVideo2);
-    var video2 = document.getElementById("myVideoPlayer2");
-     function stopVideo2() {
-          video2.pause();
-          video2.currentTime = 0;
-     };
-
-    document.getElementById("closeButton3.1").addEventListener("click", stopVideo3);
-    document.getElementById("closeButton3.2").addEventListener("click", stopVideo3);
-    var video3 = document.getElementById("myVideoPlayer3");
-     function stopVideo3() {
-          video3.pause();
-          video3.currentTime = 0;
-     };
-
-    document.getElementById("closeButton4.1").addEventListener("click", stopVideo4);
-    document.getElementById("closeButton4.2").addEventListener("click", stopVideo4);
-    var video4 = document.getElementById("myVideoPlayer4");
-     function stopVideo4() {
-          video4.pause();
-          video4.currentTime = 0;
-     };
-
 });
-
-
